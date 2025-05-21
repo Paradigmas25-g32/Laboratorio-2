919abeb28d06f82bbf610f006a1d6a06874b5143
Matías E. Kühn <matiksec23@proton.me>
2025-05-20 23:16:32 -0300

    Implementación de GeneralParser.java

--- src/parser/GeneralParser.java
+++ src/parser/GeneralParser.java
@@ -1,7 +1,32 @@
 package parser;
 
-
-/*Esta clase modela los atributos y metodos comunes a todos los distintos tipos de parser existentes en la aplicacion*/
+/*
+ * Esta clase modela los atributos y metodos comunes a todos los distintos tipos de
+ * parser existentes en la aplicacion
+ * */
 public abstract class GeneralParser {
 
+    /**
+     * Método abstracto que deben implementar las clases hijas
+     * Este método se encarga de parsear una fuente dedatos y convertirla a un tipo T.
+     *
+     * @param source La fuente a parsear, que puede ser una cadena de caracteres o la ruta del archivo
+     * @return un objeto de tipo T que contiene la información parseada
+     * */
+    public abstract Object parser(String source) throws Exception;
+
+    /**
+     * Método helper para loguear errores durante el parseo.
+     *
+     * @param message El mensaje de error
+     * @param e La excepción que causó el error
+     */
+    protected void logError(String message, Exception e) {
+        System.err.println(
+            "[" + this.getClass().getSimpleName() + "] ERROR: " + message
+        );
+        if (e != null) {
+            e.printStackTrace();
+        }
+    }
 }
